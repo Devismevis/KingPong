74dc802ab12573bce7c6e516d19367c507a795a8
DevisMevis <stommeldavid@gmail.com>
2019-02-07 09:28:19 +0100

    Thread

--- KingPong/src/Feld.java
+++ KingPong/src/Feld.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JApplet;
 
 import org.eclipse.wb.swt.SWTResourceManager;
 import java.awt.Color;
@@ -17,6 +18,10 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelListener;
 import java.awt.event.MouseWheelEvent;
+import java.awt.Graphics;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.border.LineBorder;
 
 
 public class Feld extends JFrame {
@@ -26,7 +31,15 @@ int größe = 15;
 int Count = 0;
 int possy3 = 208;
 
-// ;_;
+int ballSpeedX = 1;
+int ballSpeedY = 1;
+int ballRadius = 20;
+int posX;		
+int posY;
+int width = 450;
+int height = 300;
+int yes= 1;
+JLabel ball;
 
 	private JPanel contentPane;
 
@@ -36,6 +49,7 @@ int possy3 = 208;
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
+				Thread move = new Thread (this);
 				try {
 					Feld frame = new Feld();
 					frame.setVisible(true);
@@ -45,6 +59,7 @@ int possy3 = 208;
 		
 			}
 		});
+		
 	}
 
 	/**
@@ -58,37 +73,33 @@ int possy3 = 208;
 		contentPane = new JPanel();
 		
 		contentPane.setBackground(Color.BLACK);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new LineBorder(Color.CYAN, 2));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		 
+		  ball = new JLabel("");
+		 ball.setIcon(new ImageIcon("C:\\Users\\davids3\\Desktop\\F5OqdKdHDwu.png"));
+		 ball.setBounds(204, 128, 16, 16);
+		 contentPane.add(ball);
 		
-		Canvas spl2 = new Canvas();
+		 Canvas spl2 = new Canvas();
+		 spl2.setBackground(Color.BLUE);
+		 spl2.setBounds(409, 116, 15, 55);
+		 contentPane.add(spl2);
 		
+		  Canvas spl1 = new Canvas();
+		  spl1.setBackground(Color.RED);
+		  spl1.setBounds(10, 116, 15, 55);
+		  contentPane.add(spl1);
+		  
+		  Canvas linie = new Canvas();
+		  linie.setBackground(Color.WHITE);
+		  linie.setBounds(210, 0, 3, 261);
+		  contentPane.add(linie);
 		
-		spl2.setBackground(Color.BLUE);
-		spl2.setBounds(409, 116, 15, 55);
-		contentPane.add(spl2);
+		  
 		
-		Canvas spl1 = new Canvas();
-		
-		
-		
-		spl1.setBackground(Color.RED);
-		spl1.setBounds(10, 116, 15, 55);
-		contentPane.add(spl1);
-		
-		Canvas canvas = new Canvas();
-		canvas.setBackground(Color.WHITE);
-		canvas.setBounds(210, 0, 3, 261);
-		contentPane.add(canvas);
-		
-		Canvas Shlook = new Canvas();
-		Shlook.setBackground(Color.WHITE);
-		Shlook.setBounds(208, 128, 15, 15);
-		contentPane.add(Shlook);
-		
-		
-	
+	//bewegen
 		addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
@@ -125,16 +136,41 @@ int possy3 = 208;
 						
 					}});
 				
-			  
+				
+	runo();			
 		
 		
-	}	
+	}
 	
-	
-	public void move1() {
-		  while (Count < 20) {
-			  possy3 = possy3 - 5;
-			  Shlook.setBounds(208, 128, 15, 15);
-}}}
+
+	public void runo() {
+		System.out.println("yes");
+		Thread Thread = new Thread();
+		
+try {	   
+	        while (true) {
+	          
+	            posX += ballSpeedX;
+	            posY += ballSpeedY;
+	            ball.setLocation(posX,posY);
+	            if (posX < 0) {
+	                ballSpeedX = -ballSpeedX; 
+	                posX = 0; 
+	            } else if (posX + ballRadius > width) {
+	                ballSpeedX = -ballSpeedX;
+	                posX = width - ballRadius;
+
+	                posY = 0;
+	            } else if (posY + ballRadius > height) {
+	                ballSpeedY = -ballSpeedY;
+	                posY = height - ballRadius;
+	            }
+	         
+	          Thread.sleep(100);
+	        }
+}catch(InterruptedException e){}
+	}
+	}
+
 		
 
